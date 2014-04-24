f7dff4a8e410070666e3c1c9f60b7650d79ef520
Jonathan Sick <jonathansick@mac.com>
2014-04-24 14:28:48 -0400

    Update starplex_twomicron.py script for new ingest API

--- scripts/starplex_twomicron.py
+++ scripts/starplex_twomicron.py
@@ -6,40 +6,40 @@ Script to ingest 2MASS data into the Staplex database
 
 import argparse
 
-from starplex.database.connection import connect
-from starplex.publicdata.twomicron import TwoMassPSCIngest
+from astropy import log
+from starplex.database import connect, Session, create_all
+from starplex.ingest import TwoMassPSCIngest
 
 
 def main():
     parser = argparse.ArgumentParser(
-            description="Ingest 2MASS PSC into Starplex")
+        description="Ingest 2MASS PSC into Starplex")
     parser.add_argument('data_dir', action='store',
-            help="Directory with psc_*.gz files")
+        help="Directory with psc_*.gz files")
     parser.add_argument('--name', action='store', default='starplex',
-            help="Database name")
+        help="Database name")
     parser.add_argument('--user', action='store', default='starplex',
-            help="Database user")
+        help="Database user")
     parser.add_argument('--pw', action='store', default=None,
-            help="Database password")
+        help="Database password")
     parser.add_argument('--url', action='store', default='localhost',
-            help="Database URL")
+        help="Database URL")
     parser.add_argument('--port', action='store', default=5432, type=int,
-            help="Database port")
+        help="Database port")
     parser.add_argument('--ra', action='store', nargs=2,
-            default=[0., 360.], type=float,
-            help="Min and max RA range")
+        default=[0., 360.], type=float,
+        help="Min and max RA range")
     parser.add_argument('--dec', action='store', nargs=2,
-            default=[-90., 90.], type=float,
-            help="Min and max Dec range")
+        default=[-90., 90.], type=float,
+        help="Min and max Dec range")
     args = parser.parse_args()
 
-    engine, Session = connect(host=args.url, port=args.port, user=args.user,
-            name=args.name, password=args.pw)
+    log.setLevel('INFO')
+    connect(user=args.user, name=args.name)
     session = Session()
-
-    ingester = TwoMassPSCIngest(session, args.data_dir)
-    ingester.ingest_in_range(args.ra, args.dec)
-    session.commit()
+    create_all()
+    tm_ingester = TwoMassPSCIngest(session, args.data_dir)
+    tm_ingester.ingest_region('2MASS_PSC', [7.5, 17], [36, 47])
 
 
 if __name__ == '__main__':
