79d4364d5dfe755092d071f32d2aaa9669791b28
Jonathan Sick <jonathansick@mac.com>
2014-05-18 17:01:44 -0400

    Add intercal prepare_network
    This method finds all overlapping pairs of catalogs for a single
    bandpass, and adds each overlap (an edge in a network) to the
    IntercalEdge table.

---
+++ starplex/intercal.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Pipeline for computing inter-catalog zeropoint calibrations by minimizing
+field-to-field zeropoint differences.
+"""
+
+from .database import Catalog, Bandpass, Observation, IntercalEdge
+from .database import CatalogOverlaps
+
+
+def prepare_network(session, bandpass):
+    """Find the network of overlapping fields for this bandpass and prepare
+    rows in the `intercal_edge` table.
+    """
+    catalogs = session.query(Catalog).\
+        join(CatalogStar).\
+        join(Observation).\
+        filter(Observation.bandpass == bandpass).\
+        group_by(Catalog.id)
+    network_catalogs = session.query(Catalog.id).\
+        join(CatalogStar).\
+        join(Observation).\
+        filter(Observation.bandpass == bandpass).\
+        group_by(Catalog.id).\
+        all()
+
+    for catalog in catalogs:
+        print catalog.name
+        # Get overlapping fields
+        overlaps = CatalogOverlaps(session, catalog)
+        overlaps.query = overlaps.query.\
+            filter(Catalog.id.in_(network_catalogs))
+        overlap_count = overlaps.count
+        print "overlap count", overlaps.count
+        if overlap_count == 0:
+            continue
+
+        # For each overlap, ensure the edge is unique (or its inverse) and
+        # add it to IntercalEdge
+        for to_catalog in overlaps.catalogs:
+            if IntercalEdge.edge_exists(session,
+                                           catalog, to_catalog,
+                                           bandpass):
+                # skip this
+                continue
+
+            edge = IntercalEdge(catalog, to_catalog, bandpass, None, None)
+            session.add(edge)
